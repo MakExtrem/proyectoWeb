4474db90252597833ff4af358bdf070da8a45f5f
MakExtrem <elvislevano95@gmail.com>
2024-05-27 03:12:27 -0500

    Se termino la agenda de cita y se limito el medico por especialidad

--- src/main/resources/static/assets/js/agendarlistar.js
+++ src/main/resources/static/assets/js/agendarlistar.js
@@ -1,37 +1,39 @@
+const ListaNombreEspecialidad = async () => {
+    const response = await fetch("http://localhost:8080/especialidad");
+    const especialidad = await response.json();
 
-        const ListaNombreEspecialidad = async () => {
-            const response = await fetch("http://localhost:8080/especialidad");
-            const especialidad = await response.json();
+    let option = `<option value="" id="especialidad">Elige la Especialidad</option>`;
+    especialidad.forEach((especialidad) => {
+        console.log(especialidad);
+        option += `<option value="${especialidad.id}" id="especalidad" name="">${especialidad.nombre}</option>`;
+    });
+option_especialidades.innerHTML = option;
 
-            let option = `<option value="" id="">Elige la Especialidad</option>`;
-            especialidad.forEach((especialidad, index) => {
-            console.log(especialidad);
-                option += `<option value="${especialidad.id}" id="${especialidad.id}">${especialidad.nombre}</option>`;
-            });
-            // document.getElementById("tableBody_Users").innerHTML = tableBody;
-            option_especialidades.innerHTML = option;
-        };
+};
 
-        window.addEventListener("load", function () {
-            ListaNombreEspecialidad();
+const ListaNombreMedico = async (especialidadId) => {
+    const response = await fetch("http://localhost:8080/medico");
+    const medico = await response.json();
 
-        });
+    let option = `<option value="" id="">Elige tu Medico</option>`;
+    medico.forEach((medico) => {
+        if(medico.id_Especialidad == especialidadId) {
+            option += `<option value="${medico.id}" id="id_Medico">${medico.nombre} ${medico.apellido}</option>`;
+        }
+    });
 
+    option_medico.innerHTML = option;
+};
 
-        const ListaNombreMedico = async () => {
-            const response = await fetch("http://localhost:8080/medico");
-            const medico = await response.json();
+window.addEventListener("load", function () {
+    ListaNombreEspecialidad();
 
-            let option = `<option value="" id="">Elige tu Medico</option>`;
-            medico.forEach((medico, index) => {
-            console.log(medico);
-                option += `<option value="${medico.id}" id="id_Medico">${medico.nombre}</option>`;
-            });
-            // document.getElementById("tableBody_Users").innerHTML = tableBody;
-            option_medico.innerHTML = option;
-        };
-
-        window.addEventListener("load", function () {
-            ListaNombreMedico();
-
-        });
+    option_especialidades.addEventListener("change", function () {
+        const espec = this.value;
+        if (espec) {
+            ListaNombreMedico(espec);
+        } else {
+            option_medico.innerHTML = `<option value="" id="">Elige tu Medico</option>`;
+        }
+    });
+});
